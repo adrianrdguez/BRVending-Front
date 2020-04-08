11e3269f9b3c6912c1f10dfc926643ba3be9ee7f
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-04-08 12:30:55 +0100

    Sign up & Login funcionando con backend

---
+++ src/components/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-card width="400px" class="ml-5 mt-5 mx-a">
+      <v-card-title class="pb-0">
+        <h1>Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="Username"
+            v-model="username"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+
+          <v-text-field
+            label="Password"
+            v-model="userPassword"
+            :type="showPassword ? 'text' : 'password'"
+            prepend-icon="mdi-lock"
+            :rules="passwordRule"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="info" @click="loginMethod">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import APIServices from "../services/api.js";
+export default {
+  data() {
+    return {
+      username: "",
+      showPassword: false,
+      userPassword: "",
+      passwordRule: [
+        v => !!v || "Password is required",
+        v => v.length >= 10 || "Password must be more than 10 characters"
+      ]
+    };
+  },
+  methods: {
+    loginMethod() {
+      const user = {
+        name: this.username,
+        password: this.userPassword
+      };
+      APIServices.login(user)
+        .then(response => {
+          localStorage.setItem("token", response.token);
+          this.$router.push("/home");
+        })
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
